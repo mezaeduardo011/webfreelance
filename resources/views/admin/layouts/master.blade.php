0b8d4dfd103d8e81acfa966dc2f3802205f726e1
Ariadna <ariadnabonive@gmail.com>
2017-04-03 12:05:00 -0400

    registro

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Cache-control" content="max-age=2592000">
 
-    <title>VairoWork | Sitio web</title>
+    <title>Sitio web</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
